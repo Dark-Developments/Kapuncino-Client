bf7acb0059fff894cb2980798c60f2bbf877cfd4
Antho <anthony.ashworth007@gmail.com>
2023-03-18 17:25:34 +0000

    Baritone Command

---
+++ src/main/java/coffee/client/feature/command/impl/BaritoneCommand.java
@@ -0,0 +1,32 @@
+package coffee.client.feature.command.impl;
+
+import baritone.api.BaritoneAPI;
+import coffee.client.feature.command.Command;
+import coffee.client.feature.command.coloring.ArgumentType;
+import coffee.client.feature.command.coloring.PossibleArgument;
+
+import java.util.stream.Stream;
+
+public class BaritoneCommand extends Command {
+    public BaritoneCommand() {
+        super("Baritone", "Runs a baritone command", "baritone", "b");
+    }
+
+    @Override
+    public PossibleArgument getSuggestionsWithType(int index, String[] args) {
+        if (index == 0) {
+            return new PossibleArgument(ArgumentType.STRING, "<Baritone command>");
+        }
+        Stream<String> stringStream = BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().tabComplete(String.join(" ", args));
+        String[] strings = stringStream.toArray(String[]::new);
+        return new PossibleArgument(ArgumentType.STRING, strings.length == 0 ? new String[] { "<Baritone argument>" } : strings);
+    }
+
+    @Override
+    public void onExecute(String[] args) {
+        boolean executedAnything = BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute(String.join(" ", args));
+        if (!executedAnything) {
+            error("Command not found");
+        }
+    }
+}
